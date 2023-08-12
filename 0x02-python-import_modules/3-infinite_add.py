7a201b7f70d1b5910ddf19cbfa54e716b951c994
berrami mokhtara <berramimokhtara@gmail.com>
2023-08-11 17:36:01 -0700

    ag

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     import sys
 
-    total = 0
+    tot = 0
+
     for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+        tot += int(sys.argv[i + 1])
+
+    print("{}".format(tot))
