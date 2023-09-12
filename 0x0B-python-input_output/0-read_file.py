b15ab9da91d97cce701134e96d5ca489195e5ed7
mokhtara berrami <berramimokhtara@gmail.com>
2023-09-12 15:23:02 -0700

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,6 @@
-!/usr/bin/python3
-"""
-0-read_file module
-"""
+#!/usr/bin/python3
 
 
 def read_file(filename=""):
-    """
-    read_file - reads a text file (UTF8) and prints it to stdout
-    Args:
-        filename: name of the file
-    """
     with open(filename, "r", encoding="UTF-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
